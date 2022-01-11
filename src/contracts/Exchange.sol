98e148b7994032e06ad3aecf4ee3d117d54d6b1b
Mauricio Lozano <mlozano0322@gmail.com>
2022-01-10 19:07:25 -0600

    Successful test of tracking token deposits on the exchange

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -1,8 +1,28 @@
+pragma solidity ^0.5.0;
+
+import "./Token.sol";
+
 //Deposit & Withdraw Funds
 //Manage Orders - Make or Cancel
 //Handle Trades - Charge fees
 
-pragma solidity ^0.5.0;
+
+// TODO:
+// [X] Set the fee account
+// [ ] Deposit Ether
+// [ ] Withdraw Ether
+// [ ] Deposit tokens
+// [ ] Withdraw tokens
+// [ ] Check balances
+// [ ] Make order
+// [ ] Cancel order
+// [ ] Fill order 
+// [ ] Charge fees
+
+
+
+
+
 
 contract Exchange {
 	//Variables
@@ -14,17 +34,18 @@ contract Exchange {
 		feeAccount = _feeAccount;
 		feePercent = _feePercent;
 	}
+
+	function depositToken(address _token, uint256 _amount) public {
+		// Which token? --any ERC20, paramenter of _token
+
+		// How much of that token? parameter of _amount
+
+		// Send toeksn to this contract
+		Token(_token).transferFrom(msg.sender, address(this), _amount);
+
+		// Manage deposit -- update balance
+
+		// Emit event
+
+	}
 }
-
-// TODO:
-// [ ] Set the fee account
-// [ ] Deposit Ether
-// [ ] Withdraw Ether
-// [ ] Deposit tokens
-// [ ] Withdraw tokens
-// [ ] Check balances
-// [ ] Make order
-// [ ] Cancel order
-// [ ] Fill order 
-// [ ] Charge fees
-
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,18 +1,26 @@
 import {tokens, EVM_REVERT} from './helpers'
 
+const Token = artifacts.require('./Token')
 const Exchange = artifacts.require('./Exchange')
 
 require('chai')
 	.use(require('chai-as-promised'))
 	.should()
 
-contract('Exchange', ([deployer, feeAccount]) => {
+contract('Exchange', ([deployer, feeAccount, user1]) => {
+	let token
 	let exchange
 	const feePercent = 10
 
 	beforeEach(async () => {
+		// Deploy token
+		token = await Token.new()
+		// Transfer some tokens to user1
+		token.transfer(user1, tokens(100), {from: deployer})
 
+		// Deploy exchange
 		exchange = await Exchange.new(feeAccount, feePercent)
+
 	})
 
 	describe('deployment', () => {
@@ -25,5 +33,35 @@ contract('Exchange', ([deployer, feeAccount]) => {
 			const result = await exchange.feePercent()
 			result.toString().should.equal(feePercent.toString())
 		})
+	})
+
+	describe('depositing tokens', () => {
+		let result
+		let amount
+
+		beforeEach(async () =>{
+			amount = tokens(10)
+			await token.approve(exchange.address, amount, {from: user1})
+			const result = await exchange.depositToken(token.address, amount, {from: user1})
+		})
+
+		describe('success', () => {
+			it('tracks the token deposit', async () => {
+			// Check exchange's token balance
+			let balance
+			balance = await token.balanceOf(exchange.address)
+			balance.toString().should.equal(amount.toString())
+		})
+
+		})
+
+		describe('failure', () => {
+			it('tracks failure', async () => {
+			
+		})
+		})
+
+		
+		
 	})
 })
