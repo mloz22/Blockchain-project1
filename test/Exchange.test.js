81f7013b0632d55a7a78bb5906a0576accc2dd54
Mauricio Lozano <mlozano0322@gmail.com>
2022-01-11 19:09:51 -0600

    Finished testing for cancelling orders

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -7,7 +7,7 @@ require('chai')
 	.use(require('chai-as-promised'))
 	.should()
 
-contract('Exchange', ([deployer, feeAccount, user1]) => {
+contract('Exchange', ([deployer, feeAccount, user1, user2]) => {
 	let token
 	let exchange
 	const feePercent = 10
@@ -291,15 +291,22 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
 				event.tokenGive.should.equal(ETHER_ADDRESS, 'tokenGive is correct')
 				event.amountGive.toString().should.equal(ether(1).toString(), 'amountGive is correct')
 				event.timestamp.toString().length.should.be.at.least(1, 'timestamp is present')
-
-
-			})
+				})
 
 			})
 
 			describe('failure', async () => {
-
+				it('rejects invalid order ids', async () => {
+					const invalidOrderId = 99999
+					await exchange.cancelOrder(invalidOrderId, {from: user1}).should.be.rejectedWith(EVM_REVERT)
+				})
+				
+				it('rejects unauthorized cancellations', async () => {
+					// Try to cancel the order from another user
+					await exchange.cancelOrder('1', {from: user2}).should.be.rejectedWith(EVM_REVERT)
+				})
 			})
+		
 		})
 	})
 
